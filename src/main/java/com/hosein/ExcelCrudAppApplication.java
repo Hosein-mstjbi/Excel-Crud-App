0875df7048e753b23fe081d6e684ffba517bddc6
Hosein Mostajabi <Hosein.mostajabii@gmail.com>
2026-09-06 19:48:40 +0330

    The project and packages were created.

---
+++ src/main/java/com/hosein/ExcelCrudAppApplication.java
@@ -0,0 +1,13 @@
+package com.hosein;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelCrudAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcelCrudAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hosein/ExcelCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hosein;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelCrudAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
